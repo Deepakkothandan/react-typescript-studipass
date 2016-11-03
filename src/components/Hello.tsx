02e06eaf78891039ad9d7d635df4b65d94a6ca13
Deepak Kothandan <deepak.kdy@gmail.com>
2016-11-03 01:12:12 +0100

    material-ui working

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import * as classNames from 'classnames';
-
+import RaisedButton from 'material-ui/RaisedButton';
 export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
         return <div>
                   <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+                  <RaisedButton label="Default" />
               </div>
     }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import { Hello } from "./components/Hello";
+const App = () => (
+  <MuiThemeProvider>
+    <Hello compiler="Studipass" framework="React" />
+  </MuiThemeProvider>
+);
 
 ReactDOM.render(
-    <Hello compiler="Studipass" framework="React" />,
+     <App />  ,
     document.getElementById("content")
 );
